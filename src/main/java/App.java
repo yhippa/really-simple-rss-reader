7856601b193f704c53c6a83aaf2bfcc8db5d5baf
Richard Yhip <richard.yhip@gmail.com>
2020-06-24 21:53:39 -0400

    Added Maven

---
+++ src/main/java/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) {
+        getMessage();
+    }
+
+    private static void getMessage() {
+        System.out.println("Hello, world!");
+    }
+}
